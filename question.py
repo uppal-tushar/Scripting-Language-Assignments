ebc288dd6ab3b20404c0f394b27ccb453330cd0d
uppal-tushar <35487598+uppal-tushar@users.noreply.github.com>
2024-01-19 15:39:23 -0600

    Add files via upload

---
+++ question.py
@@ -0,0 +1,15 @@
+import csv
+programming = [["Python", "cop1000", 3],
+
+               ["Java", "cop1020", 3],
+
+               ["HTML5", "cop1040", 3]]
+with open('prog.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerows(programming)
+
+
+
+
+
+    
